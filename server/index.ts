d236b6afa6d1d38037f5acc3b0f4968140a81dc4
Yuvaraj S <yuvaraj35brave608@gmail.com>
2025-11-29 11:19:34 +0530

    Fix deployment issue

--- server/index.ts
+++ server/index.ts
@@ -73,7 +73,7 @@ app.use((req, res, next) => {
   // importantly only setup vite in development and after
   // setting up all the other routes so the catch-all route
   // doesn't interfere with the other routes
-  if (process.env.NODE_ENV === "production") {
+  if (process.env.NODE_ENV === "production" || process.env.VERCEL) {
     serveStatic(app);
   } else {
     const { setupVite } = await import("./vite");
