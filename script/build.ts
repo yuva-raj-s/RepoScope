96bd962e63848a40af929ac82b0ffa32b804b49c
Yuvaraj S <yuvaraj35brave608@gmail.com>
2025-11-29 11:50:16 +0530

    Fix build.ts

--- script/build.ts
+++ script/build.ts
@@ -36,7 +36,9 @@ async function buildAll() {
   await rm("dist", { recursive: true, force: true });
 
   console.log("building client...");
-  await viteBuild();
+  await viteBuild({
+    root: "client",
+  });
 
   console.log("building server...");
   const pkg = JSON.parse(await readFile("package.json", "utf-8"));
